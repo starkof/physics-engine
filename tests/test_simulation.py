c94cc4384c84f617ecd39a5b10f7edcaef35c0ab
starkof <saystephan@gmail.com>
2021-11-21 01:27:56 +0000

    stubbed tests

---
+++ tests/test_simulation.py
@@ -0,0 +1,10 @@
+import unittest
+from src.physics_engine import *
+
+
+class TestSimulation(unittest.TestCase):
+    def test_SimulationResult(self):
+        raise NotImplemented
+
+    def test_simulate(self):
+        raise NotImplemented
--- tests/tests_kinematic_equations.py
+++ tests/tests_kinematic_equations.py
@@ -11,6 +11,9 @@ class TestEquations(unittest.TestCase):
         self.assertEqual(5, velocity(5, 0, 0))
         self.assertEqual(11, velocity(5, 2, 3))
 
+    def test_velocity_vector(self):
+        raise NotImplemented
+
     def test_distance_scalar(self):
         self.assertEqual(0, distance(0, 0, 0, 0))
         self.assertEqual(0, distance(0, 0, 1, 0))
@@ -20,13 +23,22 @@ class TestEquations(unittest.TestCase):
         self.assertEqual(6, distance(0, 6, 0, 0))
         self.assertEqual(16, distance(2, 6, 3, 2))
 
+    def test_distance_vector(self):
+        raise NotImplemented
+
     def test_potential_energy_scalar(self):
         self.assertEqual(0, potential_energy(0, 0))
         self.assertEqual(const.g, potential_energy(1, 1))
         self.assertEqual(58.8399, potential_energy(2, 3))
+
+    def test_potential_energy_vector(self):
+        raise NotImplemented
 
     def test_kinetic_energy_scalar(self):
         self.assertEqual(0, kinetic_energy(0, 0))
         self.assertEqual(0.5, kinetic_energy(1, 1))
         self.assertEqual(2, kinetic_energy(1, 2))
         self.assertEqual(6, kinetic_energy(3, 2))
+
+    def test_kinetic_energy_vector(self):
+        raise NotImplemented
